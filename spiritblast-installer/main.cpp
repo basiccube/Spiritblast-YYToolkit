8d62644669d1395d5a0f45211c7403acb7a69574
basiccube <67266535+basiccube@users.noreply.github.com>
2026-01-08 17:45:05 +0200

    The installer now works
    Except the chance it just doesn't because

--- spiritblast-installer/main.cpp
+++ spiritblast-installer/main.cpp
@@ -1,7 +1,9 @@
 #include <string>
 #include <filesystem>
-#include <stdio.h>
+#include <cstdio>
+#include <cstdlib>
 #include <Windows.h>
+#include <atlstr.h>
 #include <nfd.hpp>
 
 using namespace std;
@@ -10,21 +12,21 @@ using namespace std;
 #define AURIEPATCHER_URL "https://github.com/AurieFramework/Aurie/releases/download/v2.0.0b/AuriePatcher.exe"
 #define YYTOOLKIT_URL "https://github.com/AurieFramework/YYToolkit/releases/download/v5.0.0a/YYToolkit.dll"
 
-// This doesn't work for some reason
-//#define SPIRITBLAST_URL "https://github.com/basiccube/Spiritblast/releases/latest/download/AB-Mod.dll"
+// This doesn't work because there are only pre-release releases and no releases with Spiritblast as the name right now
+//#define SPIRITBLAST_URL "https://github.com/basiccube/Spiritblast/releases/latest/download/Spiritblast.dll"
 #define SPIRITBLAST_URL "https://github.com/basiccube/Spiritblast/releases/download/0.3/AB-Mod.dll"
 
 LPWSTR convert_string(string str)
 {
-	wchar_t wstr[512];
-	mbstowcs(wstr, str.c_str(), str.length());
-
+	CA2WEX wstr(str.c_str());
 	LPWSTR lpstr = wstr;
 	return lpstr;
 }
 
 bool exec_and_wait(string cmdLine)
 {
+	printf("Running : %s\n", cmdLine.c_str());
+
 	// I fucking hate the Win32 API
 	STARTUPINFO startInfo;
 	PROCESS_INFORMATION procInfo;
@@ -47,28 +49,57 @@ bool exec_and_wait(string cmdLine)
 	return true;
 }
 
-bool download_file(string url, string outPath)
+#define MAX_DOWNLOAD_ATTEMPTS 3
+bool download_file(string url, string outPath, int attempt = 1)
 {
+	if (attempt > MAX_DOWNLOAD_ATTEMPTS)
+	{
+		printf("Attempted to download a total of %i times but none have succeeded.\n", attempt - 1);
+		return false;
+	}
+
 	printf("URL : %s\n", url.c_str());
 	printf("Output destination : %s\n", outPath.c_str());
+	printf("Attempt : %i\n", attempt);
 
-	string curlCmd = "curl --retry 3 --create-dirs -L -o \"" + outPath + "\" " + url;
+	string curlCmd = "curl --retry 3 --progress-bar --create-dirs -L -o \"" + outPath + "\" \"" + url + "\"";
 	if (!exec_and_wait(curlCmd))
-		return false;
+	{
+		printf("The download failed, retrying...\n");
+		return download_file(url, outPath, ++attempt);
+	}
 
 	// Check if the file at the very least was created
 	if (!filesystem::exists(outPath))
 	{
-		printf("File %s doesn't exist, the download might have failed.\n", outPath.c_str());
-		return false;
+		printf("File %s doesn't exist, the download might have failed.\nRetrying download...\n", outPath.c_str());
+		return download_file(url, outPath, ++attempt);
+	}
+
+	// Check if it at least downloaded something that isn't "Not Found"
+	printf("File downloaded, size : %ji\n", filesystem::file_size(outPath));
+	if (filesystem::file_size(outPath) < 2048)
+	{
+		printf("File %s didn't download properly, retrying...\n", outPath.c_str());
+		return download_file(url, outPath, ++attempt);
 	}
 	
 	return true;
 }
 
+int wait_for_input()
+{
+	int c = getchar();
+	return c;
+}
+
 int quit(int result)
 {
+	printf("\nPress any key to exit.\n");
+	wait_for_input();
+
 	NFD::Quit();
+	exit(result);
 	return result;
 }
 
@@ -105,10 +136,13 @@ int main(int argc, char *argv[])
 	else
 		printf("ANTONBLAST executable found\n");
 
-	printf("Downloading AurieCore.dll...\n");
+	printf("\nThe installer will now download all files required for the mod to work.\nPress any key to continue.\n");
+	wait_for_input();
+
 	string aurieCorePath = outputStr + "\\mods\\Native\\AurieCore.dll";
 	if (!filesystem::exists(aurieCorePath))
 	{
+		printf("Downloading AurieCore.dll...\n");
 		if (!download_file(AURIECORE_URL, aurieCorePath))
 		{
 			printf("Failed to download AurieCore!\n");
@@ -116,12 +150,12 @@ int main(int argc, char *argv[])
 		}
 	}
 	else
-		printf("AurieCore.dll already exists, moving on\n");
+		printf("AurieCore.dll found\n");
 
-	printf("Downloading AuriePatcher.exe...\n");
 	string auriePatcherPath = outputStr + "\\AuriePatcher.exe";
 	if (!filesystem::exists(auriePatcherPath))
 	{
+		printf("Downloading AuriePatcher.exe...\n");
 		if (!download_file(AURIEPATCHER_URL, auriePatcherPath))
 		{
 			printf("Failed to download AuriePatcher!\n");
@@ -129,12 +163,12 @@ int main(int argc, char *argv[])
 		}
 	}
 	else
-		printf("AuriePatcher.exe already exists, moving on\n");
+		printf("AuriePatcher.exe found\n");
 
-	printf("Downloading YYToolkit.dll...\n");
 	string yyToolkitPath = outputStr + "\\mods\\Aurie\\YYToolkit.dll";
 	if (!filesystem::exists(yyToolkitPath))
 	{
+		printf("Downloading YYToolkit.dll...\n");
 		if (!download_file(YYTOOLKIT_URL, yyToolkitPath))
 		{
 			printf("Failed to download YYToolkit!\n");
@@ -142,10 +176,10 @@ int main(int argc, char *argv[])
 		}
 	}
 	else
-		printf("YYToolkit.dll already exists, moving on\n");
+		printf("YYToolkit.dll found\n");
 
-	printf("Downloading Spiritblast.dll...\n");
 	string spiritblastPath = outputStr + "\\mods\\Aurie\\Spiritblast.dll";
+	printf("Downloading Spiritblast.dll...\n");
 	if (!download_file(SPIRITBLAST_URL, spiritblastPath))
 	{
 		printf("Failed to download Spiritblast!\n");
@@ -159,7 +193,7 @@ int main(int argc, char *argv[])
 		return quit(1);
 	}
 
-	printf("Installation done.\nIf everything went well then launching the game should now have a command prompt window open.\n");
+	printf("\nInstallation done.\nIf everything went well then launching the game should now have a command prompt window open.\n");
 	printf("If this doesn't happen, then something might've went wrong. Maybe try the manual installation method next.\n");
 	
 	return quit(0);
